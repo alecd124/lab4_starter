ea13ad9d8573301ba52b6473dc9aec7c64cb8483
AJ <142957187+alecd124@users.noreply.github.com>
2025-04-30 03:16:12 -0700

    fix lab4.js pt 2

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -1,36 +1,35 @@
-/**
- * 
- * @param {*} num1 - First number to add. 
- * @param {*} num2 - Second number to add. 
- * @param {*} add - Boolean value to tell the function what to do. 
- * @returns The sum of the two numbers if add is true, otherwise false.
- */
+/
+ 
+@param {*} num1, first number to add.
+@param {*} num2, second number to add.
+@param {*} add, boolean value to tell the function what to do.
+@returns The sum of the two numbers if add is true and false otherwise.*/
 function sumValues(num1, num2, add) {
     if (add) {
         let result = 0;
         result = num1 + num2;
         return result;
-    } else {
+    }
+    else {
         return !add;
     }
 }
 
-/**
- * 
- * @param {*} prices - An array of the original prices.
- * @param {*} discount - A number between 0–1 representing the discount. 
- * @returns An array of each price's new price after the discount is applied, or false if prices array is empty.
- */
+/
+ 
+@param {*} prices, an array of the original price.
+@param {*} discount, a number between 0-1 to represent the discount.
 function discountPrices(prices, discount) {
-    if (prices.length === 0) return false;
-
-    const discounted = [];
-    for (let i = 0; i < prices.length; i++) {
-        let discountedPrice = prices[i] * (1 - discount);
+    
+    const discounted = []
+    const length = prices.length;
+    let discountedPrice = 0
+    for(let i = 0; i < length; i++) {
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
     return discounted;
 }
 
-module.exports = { sumValues, discountPrices };
+module.exports = {sumValues, discountPrices};
